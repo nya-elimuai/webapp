6ca313444f520ea60e41f3096bf6353c0d613364
Magnus <magnus@maggim.net>
2018-09-10 08:53:11 +0200

    Drag and drop db migration; drop indexes only if they exists, fixes error on startup if db already migrated

--- src/main/resources/db/migration/2000071.sql
+++ src/main/resources/db/migration/2000071.sql
@@ -2,13 +2,13 @@
 
 DELETE FROM `DbMigration` WHERE `version` = 2000071;
 
-ALTER TABLE `Project_AppCategory` DROP FOREIGN KEY `FK_adqa65g6o4ti2ct3oqw0uw1bj`;
-ALTER TABLE `Project_AppCategory` DROP INDEX `UK_adqa65g6o4ti2ct3oqw0uw1bj`;
+ALTER TABLE `Project_AppCategory` DROP FOREIGN KEY IF EXISTS `FK_adqa65g6o4ti2ct3oqw0uw1bj`;
+ALTER TABLE `Project_AppCategory` DROP INDEX IF EXISTS `UK_adqa65g6o4ti2ct3oqw0uw1bj`;
 
-ALTER TABLE `AppCategory_AppGroup` DROP FOREIGN KEY `FK_dtopf8r1vkfb4peq5n3le4lj8`;
-ALTER TABLE `AppCategory_AppGroup` DROP INDEX `UK_dtopf8r1vkfb4peq5n3le4lj8`;
+ALTER TABLE `AppCategory_AppGroup` DROP FOREIGN KEY IF EXISTS `FK_dtopf8r1vkfb4peq5n3le4lj8`;
+ALTER TABLE `AppCategory_AppGroup` DROP INDEX IF EXISTS `UK_dtopf8r1vkfb4peq5n3le4lj8`;
 
-ALTER TABLE `AppGroup_Application` DROP FOREIGN KEY `FK_4j5x3dru9r3lv2s3jigi6ryrn`;
-ALTER TABLE `AppGroup_Application` DROP INDEX `UK_4j5x3dru9r3lv2s3jigi6ryrn`;
+ALTER TABLE `AppGroup_Application` DROP FOREIGN KEY IF EXISTS `FK_4j5x3dru9r3lv2s3jigi6ryrn`;
+ALTER TABLE `AppGroup_Application` DROP INDEX IF EXISTS `UK_4j5x3dru9r3lv2s3jigi6ryrn`;
 
 
